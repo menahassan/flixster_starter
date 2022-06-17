41b44fdaee2ffff83d1c0e22638a3b1a9eaf548c
menahassan <menah9@gmail.com>
2022-06-16 22:45:31 -0700

    add searchbar

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+API_KEY = "658568773162c3aaffcb3981d4f5587b"
 const imageBaseUrl = 'https://image.tmdb.org/t/p'
+let PAGE = 1
 let currentMovie = "" 
+var movieCardsEl = ""
+
+MOVIES_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=`
+SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`
+GET_MOVIE_BY_ID_URL = `https://api.themoviedb.org/3/movie/`
+var CURRENT_URL = MOVIES_URL
+//search: https://api.themoviedb.org/3/search/company?api_key=658568773162c3aaffcb3981d4f5587b&query=QUERYHERE&page=1
+
 
 // Example image tag
 //<img class="movie-poster" src="${imageBaseUrl}/w342${movie.posterPath}" alt="${movie.title}" title="${movie.title}"/>
  
-const movies = [
+/*const movies = [
     {
         id: 338953,
         posterPath: "/8ZbybiGYe8XM4WGmGlhF0ec5R7u.jpg",
@@ -118,45 +128,19 @@ const movies = [
         overview: "Creatively unfulfilled and facing financial ruin, Nick Cage must accept a $1 million offer to attend the birthday of a dangerous superfan. Things take a wildly unexpected turn when Cage is recruited by a CIA operative and forced to live up to his own legend, channeling his most iconic and beloved on-screen characters in order to save himself and his loved ones."
     }
  ];
- 
+ */
 
 const moviesGridEl = document.querySelector("#movies-grid")
-const movieTitleEl = document.querySelector(".movie-title")
-const moviePosterEl = document.querySelector(".movie-poster")
-const movieVotesEl = document.querySelector(".movie-votes")
 const movieButtonEl = document.querySelector("#load-more-movies-btn")
-
-
-//create a movie card
-function listMovie(movie){
-    console.log(movie.title)
-    moviesGridEl.innerHTML = moviesGridEl.innerHTML + `
-    <div class="movie-card">
-        <p class="movie-title">${movie.title}</p>
-        <img class="movie-poster" src="${imageBaseUrl}/w342${movie.posterPath}" alt="${movie.overview}" title="${movie.title}"/>
-        <p class="movie-votes">Rating: ${movie.voteAverage}</p>
-    </div>
-    `
-}
-//create a movie card for each movie
-movies.forEach(listMovie)
-
-//load more movies
-function loadMoreMovies(){
-    movies.forEach(listMovie)
-}
-
-//load more movies if button is clicked
-movieButtonEl.addEventListener('click', loadMoreMovies)
-
-const movieCardsEl = document.getElementsByClassName("movie-poster")
-const modals = document.getElementsByClassName("modal");
-
 const modal = document.querySelector(".modal");
 
+const  searchButtonEl = document.getElementById("search-button")
+const  searchInputEl = document.querySelector(".search-input")
+
 function toggleModal(target) {
-    modal.classList.toggle("show-modal");
-    if(target != "none"){
+    console.log('hello')
+    //modal.classList.toggle("show-modal");
+    /*if(target != "none"){
         var title = target.title
         var overview= target.alt
         var src= target.src
@@ -172,17 +156,69 @@ function toggleModal(target) {
         `
         const closeButton = document.querySelector(".close-button");
         closeButton.addEventListener("click", function(){toggleModal("none")});
+    }*/
+}
+
+//create a movie card
+function listMovie(movie){
+    var id = movie.id
+    moviesGridEl.innerHTML = moviesGridEl.innerHTML + `
+    <div class="movie-card">
+        <p class="movie-title">${movie.title}</p>
+        <img id=${id} class="movie-poster" src="${imageBaseUrl}/w342${movie.poster_path}" alt="${movie.overview}" title="${movie.title}"/>
+        <p class="movie-votes">Rating: ${movie.vote_average}</p>
+    </div>
+    `
+    document.getElementById(id).addEventListener('click' , () => toggleModal('hello'))
+}
+//create a movie card for each movie
+async function getResults(PAGE_URL){
+    const response = await fetch(PAGE_URL)
+    const result = await response.json()
+    console.log("result: ",result.results)
+    for(var i = 0; i < result.results.length; i++){
+        listMovie(result.results[i])
     }
 }
 
-for(var i = 0; i < movieCardsEl.length; i++){
-    movieCardsEl[i].addEventListener('click', function(e) {
-        e = e || window.event;
-        var target = e.target;
-        toggleModal(target)   
-    }, false)
+//load more movies if button is clicked
+movieButtonEl.addEventListener('click', () => {
+    PAGE += 1
+    getResults(CURRENT_URL + PAGE)
+})
+
+//search
+document.getElementById("search-form").addEventListener('submit', (event) => {
+    event.preventDefault()
+    PAGE = 1
+    moviesGridEl.innerHTML = ""
+    var QUERY = searchInputEl.value
+    if(QUERY == ""){
+        getResults(MOVIES_URL + PAGE)
+    }
+    else{
+        var SEARCH = SEARCH_URL + `${QUERY}&page=${PAGE}`
+        CURRENT_URL = SEARCH
+        getResults(SEARCH)
+    }
+})
+
+window.onload = function(){
+    getResults(MOVIES_URL + PAGE)
 }
 
-/*for(var i = 0; i < movieCardsEl.length; i++){
-    movieCardsEl[i].addEventListener('click', function(){toggleModal(i)})
-}*/
+
+/*
+window.onload = function(){
+    getResults(MOVIES_URL + PAGE)
+
+    for(var i = 0; i < movieCardsEl.length; i++){
+        movieCardsEl[i].addEventListener('click', function(e) {
+            console.log('click')
+            e = e || window.event;
+            var target = e.target;
+            toggleModal(target)   
+        }, false)
+    }
+}
+*/
